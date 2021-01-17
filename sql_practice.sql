a400770543f1cf6bc1df543151a529e7ba01fe0b
Luke Becker <luke.w.becker32@gmail.com>
2021-01-17 15:51:33 -0600

    updates

--- sql_practice.sql
+++ sql_practice.sql
@@ -19,3 +19,14 @@ FROM STATION;
 
 -- Point of this above exercise was to show how to do simple math in the SELECT command, and to utilize DISTINCT.
 
+-- HackerRank SQL #5:
+
+SELECT CITY AS c, LENGTH(CITY) AS l
+FROM STATION
+ORDER BY l DESC, c DESC
+LIMIT 1;
+
+SELECT CITY AS c, LENGTH(CITY) AS l
+FROM STATION
+ORDER BY l ASC, c ASC
+LIMIT 1;
