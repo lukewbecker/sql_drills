a04fba58b5fdbdfca35a997b7b25d5b8f842e795
Luke Becker <luke.w.becker32@gmail.com>
2021-01-18 15:46:15 -0600

    progressing through SQL drills

--- sql_practice.sql
+++ sql_practice.sql
@@ -44,3 +44,25 @@ WHERE lower(substring(CITY, 1, 1)) IN ('a', 'e', 'i', 'o', 'u');
 SELECT DISTINCT CITY
 FROM STATION
 WHERE lower(substring(CITY, -1, 1)) IN ('a', 'e', 'i', 'o', 'u');
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(SUBSTRING(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+
+-- Finding The city names that don't end with a vowel:
+
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(SUBSTRING(CITY, -1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(SUBSTRING(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
+OR LOWER(SUBSTRING(CITY, -1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
+
