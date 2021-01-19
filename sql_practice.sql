f9fc4eb713a9fc7c7f86a10e497179e8fbe89180
Luke Becker <luke.w.becker32@gmail.com>
2021-01-19 10:49:04 -0600

    ran query that calculated max earnings for all employees

--- sql_practice.sql
+++ sql_practice.sql
@@ -129,3 +129,11 @@ FROM employees;
 
 -- So I learned about the ceiling function, and the replace function in this exercise.
 
+-- We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
+
+SELECT salary * months AS total_earnings, COUNT(employee_id)
+FROM employee
+GROUP BY total_earnings
+ORDER BY total_earnings DESC
+LIMIT 1;
+
