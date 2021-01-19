3cfd1b8744de89746a9bcbd156952ae91c0b3533
Luke Becker <luke.w.becker32@gmail.com>
2021-01-19 08:37:20 -0600

    updates to sql drills

--- sql_practice.sql
+++ sql_practice.sql
@@ -79,3 +79,23 @@ SELECT NAME
 FROM STUDENTS
 WHERE MARKS > 75
 ORDER BY RIGHT(NAME, 3), ID ASC;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+SELECT name
+FROM Employee
+ORDER BY name;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than 2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+
+SELECT name
+FROM employee
+WHERE salary > 2000 AND months < 10
+ORDER BY employee_id ASC;
+
+-- Query the total population of all cities in CITY where District is California.
+
+SELECT SUM(population)
+FROM city
+WHERE district LIKE "California"
+
