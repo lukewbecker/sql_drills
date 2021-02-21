f8d03836e2d9370134d3635b7f9165f6939a7052
Luke Becker <luke.w.becker32@gmail.com>
2021-02-21 07:48:07 -0600

    updates

--- sql_practice.sql
+++ sql_practice.sql
@@ -436,3 +436,11 @@ SELECT last_name, COUNT(last_name) as last_name_count
 FROM actor
 GROUP BY last_name
 ORDER BY last_name_count DESC;
+
+-- List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+
+SELECT last_name, COUNT(last_name)
+FROM actor
+GROUP BY last_name
+HAVING COUNT(last_name) > 1
+ORDER BY COUNT(last_name) DESC;
