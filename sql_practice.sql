52cf02bb53140e883ac214367e61575d0366a23b
Luke Becker <luke.w.becker32@gmail.com>
2021-03-20 10:36:27 -0500

    updates to drills

--- sql_practice.sql
+++ sql_practice.sql
@@ -485,3 +485,6 @@ WHERE cd.facilities.facid = '5'
 SELECT *
 FROM cd.facilities
 WHERE facid IN (1, 5);
+
+
+-- How can you produce a list of facilities, with each labelled as 'cheap' or 'expensive' depending on if their monthly maintenance cost is more than $100? Return the name and monthly maintenance of the facilities in question.
