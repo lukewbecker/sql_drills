c6da78b995d894c29ffe44105e9905a105d516cd
Luke Becker <luke.w.becker32@gmail.com>
2021-03-22 13:24:03 -0500

    filter by datetime fields

--- sql_practice.sql
+++ sql_practice.sql
@@ -496,5 +496,9 @@ SELECT name, CASE
 FROM cd.facilities;
 
 
--- Next question deals with more CASE statements.
+-- How can you produce a list of members who joined after the start of September 2012? Return the memid, surname, firstname, and joindate of the members in question.
+
+SELECT memid, surname, firstname, joindate
+FROM cd.members
+WHERE joindate > '2012-09-01'
 
