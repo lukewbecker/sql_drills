59dbb84f2945bbdbe82061a04c7d555f15ff6f24
Luke Becker <luke.w.becker32@gmail.com>
2021-04-09 08:04:13 -0500

    Practice using cross join vs inner join

--- sql_practice.sql
+++ sql_practice.sql
@@ -579,5 +579,14 @@ This space is being used for my data camp sql coursework
 So far this has been a good reenforcement of my learning through Codeup.
 */
 
-
+-- 5. Select fields
+SELECT c.name AS city, l.name AS language
+-- 1. From cities (alias as c)
+FROM cities AS c      
+  -- 2. Join to languages (alias as l)
+  INNER JOIN languages AS l
+    -- 3. Match on country code
+    ON c.country_code = l.code
+-- 4. Where c.name like Hyderabad
+WHERE c.name LIKE 'Hydera%';
 
