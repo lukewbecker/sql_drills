a5dd993d57171c81576125d2d68138714a8cb002
Luke Becker <luke.w.becker32@gmail.com>
2021-04-12 13:51:27 -0500

    updates including UNION clauses

--- sql_practice.sql
+++ sql_practice.sql
@@ -592,3 +592,16 @@ WHERE c.name LIKE 'Hydera%';
 
 -- Creating UNION clauses.
 
+-- Select fields
+SELECT code, year
+  -- From economies
+  FROM economies
+	-- Set theory clause
+	UNION ALL
+-- Select fields
+SELECT country_code, year
+  -- From populations
+  FROM populations
+-- Order by code, year
+ORDER BY code, year;
+
