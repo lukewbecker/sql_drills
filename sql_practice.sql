713a49b903f8ae6c4b47656cb4150a23cc431295
Luke Becker <luke.w.becker32@gmail.com>
2021-04-13 08:57:15 -0500

    updates using subqueries

--- sql_practice.sql
+++ sql_practice.sql
@@ -648,3 +648,14 @@ FROM matches_spain
 WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
       AND (awayteam_id = 8633 OR hometeam_id = 8633);
 
+
+SELECT 
+	-- Select the date, home goals, and away goals scored
+    date,
+	home_goal,
+	away_goal
+FROM  matches_2013_2014
+-- Filter for matches where total goals exceeds 3x the average
+WHERE (home_goal + away_goal) > 
+       (SELECT 3 * AVG(home_goal + away_goal)
+        FROM matches_2013_2014); 
