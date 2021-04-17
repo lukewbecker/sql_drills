539a8e8aa9d946113277796a01d79fe384e267a3
Luke Becker <luke.w.becker32@gmail.com>
2021-04-17 14:21:37 -0500

    updates

--- sql_practice.sql
+++ sql_practice.sql
@@ -696,6 +696,9 @@ FROM
 
 WHERE total_goals >= 10;
 
+/* This is a more complicated and still simple subquery that subtracts the league average goals scored
+by the overall goals scored in the season. */
+
 
 SELECT
 	-- Select the league name and average goals scored
@@ -712,3 +715,41 @@ ON l.country_id = m.country_id
 -- Only include 2013/2014 results
 WHERE season = '2013/2014'
 GROUP BY l.name;
+
+SELECT 
+	-- Select the stage and average goals from the subquery
+	s.stage,
+	ROUND(s.avg_goals,2) AS avg_goals
+FROM 
+	-- Select the stage and average goals in 2012/2013
+	(SELECT
+		 stage,
+         AVG(home_goal + away_goal) AS avg_goals
+	 FROM match
+	 WHERE season = '2012/2013'
+	 GROUP BY stage) AS s
+WHERE 
+	-- Filter the main query using the subquery
+	s.avg_goals > (SELECT AVG(home_goal + away_goal) 
+                    FROM match WHERE season = '2012/2013');
+
+-- Final code:
+
+SELECT 
+	-- Select the stage and average goals from s
+	s.stage,
+    ROUND(s.avg_goals,2) AS avg_goal,
+    -- Select the overall average for 2012/2013
+    (SELECT AVG(home_goal + away_goal) FROM match WHERE season = '2012/2013') AS overall_avg
+FROM 
+	-- Select the stage and average goals in 2012/2013 from match
+	(SELECT
+		 stage,
+         AVG(home_goal + away_goal) AS avg_goals
+	 FROM match
+	 WHERE season = '2012/2013'
+	 GROUP BY stage) AS s
+WHERE 
+	-- Filter the main query using the subquery
+	s.avg_goals > (SELECT AVG(home_goal + away_goal) 
+                    FROM match WHERE season = '2012/2013');
