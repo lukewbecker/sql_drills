d4eda32f2a41b784c9a25ad70707b0374e62739c
Luke Becker <luke.w.becker32@gmail.com>
2021-05-03 06:50:39 -0500

    adding some more drills with sql

--- sql_practice.sql
+++ sql_practice.sql
@@ -802,3 +802,22 @@ FROM
 WHERE s.avg_goals > (SELECT AVG(home_goal + away_goal)
                      FROM match AS m
                      WHERE s.stage > m.stage);
+
+
+-- Results of the El Clasico over two years:
+
+SELECT 
+	date,
+	CASE WHEN hometeam_id = 8634 THEN 'FC Barcelona' 
+         ELSE 'Real Madrid CF' END as home,
+	CASE WHEN awayteam_id = 8634 THEN 'FC Barcelona' 
+         ELSE 'Real Madrid CF' END as away,
+	-- Identify all possible match outcomes
+	CASE WHEN home_goal > away_goal AND hometeam_id = 8634 THEN 'Barcelona win!'
+        WHEN home_goal > away_goal AND hometeam_id = 8633 THEN 'Real Madrid win!'
+        WHEN home_goal < away_goal AND awayteam_id = 8634 THEN 'Barcelona win!'
+        WHEN home_goal < away_goal AND awayteam_id = 8633 THEN 'Real Madrid win!'
+        ELSE 'Tie!' END AS outcome
+FROM matches_spain
+WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
+      AND (awayteam_id = 8633 OR hometeam_id = 8633);
