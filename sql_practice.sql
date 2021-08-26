8a3dae46f12196b32d00d032414ad34bd310dd9a
Luke Becker <luke.w.becker32@gmail.com>
2021-08-26 07:23:48 -0500

    updates

--- sql_practice.sql
+++ sql_practice.sql
@@ -1025,3 +1025,20 @@ LIMIT 1;
 SELECT *
 FROM cities
 WHERE name = 'San%'
+
+SELECT
+  s.stage
+  ROUND(s.avg_goals, 2) AS avg_goal
+  (SELECT AVG(home_goal + away_goal)
+  FROM match
+  WHERE season = '2012/2013')
+FROM
+  (SELECT
+    stage, 
+    AVG(home_goal + away_goal) AS avg_goals
+    FROM match
+    WHERE season = '2012/2013'
+    GROUP BY stage) AS s
+WHERE s.avg_goals > (SELECT AVG(home_goal + away_goal)
+                     FROM match AS m
+                     WHERE s.stage > m.stage);
