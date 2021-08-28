1134e9273ec9f14b8057b1b7ce243393d327e19b
Luke Becker <luke.w.becker32@gmail.com>
2021-08-28 04:59:31 -0500

    updates

--- sql_practice.sql
+++ sql_practice.sql
@@ -1042,3 +1042,20 @@ FROM
 WHERE s.avg_goals > (SELECT AVG(home_goal + away_goal)
                      FROM match AS m
                      WHERE s.stage > m.stage);
+
+SELECT
+  s.stage
+  ROUND(s.avg_goals, 2) AS avg_goal
+  (SELECT AVG(home_goal + away_goal)
+  FROM match
+  WHERE season = '2012/2013')
+FROM
+  (SELECT
+    stage, 
+    AVG(home_goal + away_goal) AS avg_goals
+    FROM match
+    WHERE season = '2012/2013'
+    GROUP BY stage) AS s
+WHERE s.avg_goals > (SELECT AVG(home_goal + away_goal)
+                     FROM match AS m
+                     WHERE s.stage > m.stage);
