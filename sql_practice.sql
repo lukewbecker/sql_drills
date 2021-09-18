8e625af65138c287a09bec704cb9dbe2ae39a6b5
Luke Becker <luke.w.becker32@gmail.com>
2021-09-18 08:58:33 -0500

    Daily drills

--- sql_practice.sql
+++ sql_practice.sql
@@ -1094,3 +1094,11 @@ SELECT
         ELSE 'Scalene'
     END
 FROM TRIANGLES;
+
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(SUBSTRING(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
+AND LOWER(SUBSTRING(CITY, -1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
